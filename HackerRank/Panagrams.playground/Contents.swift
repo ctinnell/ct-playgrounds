9fc0510d6ed1914e9d78608acfc42f70d8dc19ed
Clay Tinnell <ctinnell@mac.com>
2016-03-07 16:02:20 -0500

    panagram solved

--- HackerRank/Panagrams.playground/Contents.swift
+++ HackerRank/Panagrams.playground/Contents.swift
@@ -1,1 +1,27 @@
+import Foundation
+
+let str1 = "We promptly judged antique ivory buckles for the next prize"
+let str2 = "We promptly judged antique ivory buckles for the prize"
+
+func isPanagram(str: String) -> Bool {
+    let lowerCaseLetters = ["a","b","c","d","e","f","g","h","i",
+        "j","k","l","m","n","o","p","q","r","s","t","u","v","w",
+        "x","y","z"]
+
+    let uniquestr = str.characters
+    let strArray = Array(Set(uniquestr)).map({String($0).lowercaseString})
+    let onlyLetters = Array(Set(strArray).intersect(lowerCaseLetters)).sort()
+    return onlyLetters == lowerCaseLetters
+}
+
+func panagramDescription(isPan: Bool) -> String {
+    return isPan ? "panagram" : "not panagram"
+}
+
+print(panagramDescription(isPanagram(str1)))
+
+print(panagramDescription(isPanagram(str2)))
+
+
+
 
