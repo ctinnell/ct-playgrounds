47549321529dbb64a29ecdc90fb0d58dff2e83cb
Clay Tinnell <ctinnell@mac.com>
2016-03-09 15:51:14 -0500

    initial completion

--- HackerRank/FunnyString.playground/Contents.swift
+++ HackerRank/FunnyString.playground/Contents.swift
@@ -15,7 +15,34 @@ func readLineAsString() -> String {
     return value
 }
 
+func asciValues(str: String) -> [Int] {
+    var values = [Int]()
+    for scalar in str.unicodeScalars {
+        values.append(Int(scalar.value))
+    }
+    return values
+}
+
+func isFunnyString(s_string: String) -> Bool {
+    var isFunny = false
+    let r_string = String(s_string.characters.reverse())
+    let s = asciValues(s_string)
+    let r = asciValues(r_string)
+    
+    for var j=1; j<=s.count; j+=2 {
+        if abs(s[j]-s[j-1]) == abs(r[j]-r[j-1]) {
+            isFunny = true
+        }
+        else {
+            isFunny = false
+            break
+        }
+    }
+    return isFunny
+}
+
 var numTests = readLineAsInt()
 for var x=0; x<numTests; x++ {
-    print(readLineAsString())
+    let s_string = readLineAsString()
+    print(isFunnyString(s_string) ? "Funny" : "Not Funny")
 }
