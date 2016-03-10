786698e45bd91d15a6bf3f3ca855311c7380efad
Clay Tinnell <ctinnell@mac.com>
2016-03-10 12:52:38 -0500

    li finished

--- HackerRank/LonelyInteger.playground/Contents.swift
+++ HackerRank/LonelyInteger.playground/Contents.swift
@@ -1,1 +1,29 @@
+import Foundation
 
+var input = ["3","1 1 2"]
+var row = 0
+
+func readLineAsInt() -> Int {
+    let value = Int(input[row])!
+    row = row + 1
+    return value
+}
+func readLineAsIntArray() -> [Int] {
+    let value = input[row].characters.split(" ").map(String.init).map({Int($0)!})
+    row = row + 1
+    return value
+}
+
+
+let numIntegers = readLineAsInt()
+let integers = readLineAsIntArray()
+var value = 0
+
+for integer in integers {
+    if integers.filter({$0 == integer}).count == 1 {
+        value = integer
+        break
+    }
+}
+
+print(value)
