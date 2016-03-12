e5b106849875181e16c23c6e8fd7b0a11b906027
Clay Tinnell <ctinnell@mac.com>
2016-03-12 08:47:55 -0500

    almost

---
+++ HackerRank/insertionSort.playground/Contents.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+var numbers = [2,4,6,8,3]
+numbers = [12,34,56, 1]
+numbers = [2,3,4,5,6,7,8,9,10,1]
+var startingIndex = numbers.count-2
+var compareValue = numbers[numbers.count-1]
+
+if numbers.count > 1 {
+    for index in (0...startingIndex).reverse() {
+        if compareValue<numbers[index] {
+            //print(compareValue,"<",numbers[index])
+            //print("index ",index)
+            numbers[index+1] = numbers[index]
+            if index == 0 {
+                numbers[index] = compareValue
+            }
+            print(numbers.map({String($0)}).joinWithSeparator(" "))
+        }
+        else {
+            numbers[index+1] = compareValue
+            print(numbers.map({String($0)}).joinWithSeparator(" "))
+            break
+        }
+    }
+}
